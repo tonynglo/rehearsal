f0dba39cdadede7147d352a220966999ffece5ad
tonynglo <tonyngng27@gmail.com>
2025-12-01 10:04:08 +0700

    Initial commit for band merch site
    Initial commit for band merch site

---
+++ script.js
@@ -0,0 +1,4 @@
+// Simple cart simulation (alert-based, not real e-commerce)
+function addToCart(item) {
+    alert(item + ' added to cart! (This is a demo. Integrate with a real platform for actual sales.)');
+}
